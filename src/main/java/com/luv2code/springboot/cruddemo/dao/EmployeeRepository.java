6992bae4f66faaa756ef1d8744b6dc1f9f03ae77
Sankar-bv <bvsankar88@gmail.com>
2022-04-11 23:18:05 +0530

    Spring Data REST Code

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package com.luv2code.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
+@RepositoryRestResource(path = "members")
+//To change the default entity name as endpoints we use @RepositoryRestResource annotation i.e. default endpoint is employees so we are changing to members
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
